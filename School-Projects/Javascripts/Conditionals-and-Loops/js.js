a36a955751df97c10f60cac89f7caf6b87060849
Hitori <113095167+Role34@users.noreply.github.com>
2022-10-17 19:28:21 +0800

    Javascript

---
+++ School-Projects/Javascripts/Conditionals-and-Loops/js.js
@@ -0,0 +1,48 @@
+for (var x=0; x<=15; x++)
+{
+    if (x === 0)
+    {
+        document.write(x + " is even <br>");
+    } else if (x % 2 === 0)
+    {
+        document.write(x + " is even <br>");
+    } else
+    {
+        document.write(x + " is odd <br>");
+    }
+}
+// There are 3 types of credit.
+
+// Number 1
+/*
+document.write("Code is working! :D <br>");
+document.write("created by https://github.com/Role34");
+*/
+
+// Number 2
+/*
+
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░██╗░░░░░░░██╗░█████╗░██████╗░███╗░░██╗██╗███╗░░██╗░██████╗░░
+░░██║░░██╗░░██║██╔══██╗██╔══██╗████╗░██║██║████╗░██║██╔════╝░░
+░░╚██╗████╗██╔╝███████║██████╔╝██╔██╗██║██║██╔██╗██║██║░░██╗░░
+░░░████╔═████║░██╔══██║██╔══██╗██║╚████║██║██║╚████║██║░░╚██╗░
+░░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██║░╚███║██║██║░╚███║╚██████╔╝░
+░░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░╚══╝░╚═════╝░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+Warning.output("If this credit is deleted then Fuck you >:D")
+*/
+/*
+document.write("Code is working! :D <br>");
+document.write("created by https://github.com/Role34 <br>");
+*/
+
+// Number 3
+/*
+Warning.output("If this credit is deleted then Fuck you >:D")
+*/
+/*
+document.write("Code is working! :D");
+document.write("<br>");
+document.write("created by https://github.com/Role34");
+*/
